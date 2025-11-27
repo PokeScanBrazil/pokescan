4e55356982c16c62fc4f03112bba9532bd8be469
guilherme <jguicf@gmail.com>
2025-11-26 20:08:25 -0400

    Starting basic project configuration

---
+++ PokéScan/PokéScan/Poke_ScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_ScanApp.swift
+//  PokéScan
+//
+//  Created by João Guilherme on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Poke_ScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokéScan/PokéScanTests/Poke_ScanTests.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_ScanTests.swift
+//  PokéScanTests
+//
+//  Created by João Guilherme on 26/11/25.
+//
+
+import Testing
+@testable import Poke_Scan
+
+struct Poke_ScanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
